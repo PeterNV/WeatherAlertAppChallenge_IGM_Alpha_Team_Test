fb3cdebb753b0c06e5a720e64d8a4a2def8a0188
PeterNV <114534711+PeterNV@users.noreply.github.com>
2025-09-29 19:02:33 -0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,11 +4,11 @@ import java.util.Properties
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-
+    
     alias(libs.plugins.kotlin.compose)
     id("org.jetbrains.kotlin.plugin.serialization") version "1.9.0"
     id ("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
-    id("io.ktor.plugin") version "2.3.5")
+    id("io.ktor.plugin") version "2.3.5"
     alias(libs.plugins.google.gms.google.services)
 }
 
